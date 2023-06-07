bdbb76fcf6e225fd1eab3a9782d12a47da82ab02
Rifat Saeed <48490833+rsaeed15@users.noreply.github.com>
2023-06-06 22:31:14 -0700

    Update loan_streamlit.py

--- loan_streamlit.py
+++ loan_streamlit.py
@@ -2,16 +2,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st 
 
-
-titanic = sns.load_dataset("titanic")
-
-fig = plt.figure(figsize=(10, 4))
-sns.countplot(x="class", data=titanic)
-
+dataset = pd.read_csv('loan_data.csv.gz', compression='gzip',low_memory=True)
+y=dataset['Status'].count_values()
+labels=['Not Safe','Default','Safe Loan']
+fig = plt.figure(figsize=(15,10))
+df = pd.DataFrame(prediction_proba, columns = labels)
+sns.barplot(data=y,order = ['Safe Loan', 'Not Safe','Default'])
+# Add figure in streamlit app
 st.pyplot(fig)
-
-
-
 
 
 
