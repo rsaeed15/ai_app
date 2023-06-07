1cf9aae4e5c0e7a409b639a04bf4bc1d9bb4de0d
Rifat Saeed <48490833+rsaeed15@users.noreply.github.com>
2023-06-06 22:34:16 -0700

    Update loan_streamlit.py

--- loan_streamlit.py
+++ loan_streamlit.py
@@ -56,7 +56,7 @@ data = {'loan': loan,
 features = pd.DataFrame(data,index=[0])
 	 
 
-dataset = pd.read_csv('C:\Machine-Learning-Channel\loan_data.csv.gz', compression='gzip',low_memory=True)
+dataset = pd.read_csv('loan_data.csv.gz', compression='gzip',low_memory=True)
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
