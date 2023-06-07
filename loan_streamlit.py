7a30c2601c0f010e9ddfcada08792b7ab65dfe35
Rifat Saeed <48490833+rsaeed15@users.noreply.github.com>
2023-06-06 22:15:54 -0700

    Update loan_streamlit.py

--- loan_streamlit.py
+++ loan_streamlit.py
@@ -15,9 +15,10 @@ st.write("Developer: Rifat Saeed")
 # Displaying images on the front end
 from PIL import Image
 image = Image.open('webPic.jpg')
-st.image(image, caption='Loan Default Probability')
 
+st.image(image, caption='Loan Default Probability')
 st.sidebar.header('User Input Parameters')
+
 loan = st.sidebar.slider('loan_amount', 0.0, 35000.0,5000.0)
 annual_income = st.sidebar.slider('annual_income', 0.0,700000.0,100000.0)
 purpose = st.sidebar.slider('purpose', 0,9,1)
@@ -26,12 +27,14 @@ emp_length=st.sidebar.slider('emp_length', 0, 11,1)
 borrower_score=st.sidebar.slider('borrower_score',0.0,1.0,0.1)
 
 col1, col2 = st.columns(2)
+
 with col1:
  genre1 = st.radio("What is term of the loan?",('36 months', '60 months'))
  if genre1 == '36 months':
    term= 0 
  else: 
    term= 1
+
 with col2:
  genre2 = st.radio("Home Ownership?",('Mortgage', 'Rent','Own'))
  if genre2 == 'Mortgage':
@@ -40,6 +43,7 @@ with col2:
    home_ownership= 2 
  else:
    home_ownership= 3 
+
 
 data = {'loan': loan,
             'term': term,
@@ -77,15 +81,43 @@ labels=['Not Safe','Default','Safe Loan']
 st.write("")
 st.write("")
 
-new_title = '<p style="font-family:Calibri;font-size:20px;color:Black;"><strong>Probabilty of Customer Loan Approval</strong></p>'
-st.markdown(new_title, unsafe_allow_html=True)
-labels=['Not Safe','Default','Safe Loan']
+#define multiple columns, add two graphs
+col1, col2 = st.columns(2)
 
-sns.set(font_scale=2)
-fig = plt.figure(figsize=(15,10))
-df = pd.DataFrame(prediction_proba, columns = labels)
-sns.barplot(data=df*100,order = ['Safe Loan', 'Not Safe','Default'])
+with col1:
+ new_title = '<p style="font-family:Calibri;font-size:20px;color:Black;"><strong>Probabilty of Customer Loan Approval</strong></p>'
+ st.markdown(new_title, unsafe_allow_html=True)
+
+ labels=['Not Safe','Default','Safe Loan']
+
+ sns.set(font_scale=2)
+ fig = plt.figure(figsize=(15,10))
+
+ df = pd.DataFrame(prediction_proba, columns = labels)
+ sns.barplot(data=df*100,order = ['Safe Loan', 'Not Safe','Default'])
 # Add figure in streamlit app
-st.pyplot(fig)
+ st.pyplot(fig)
+
+with col2:
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+  st.write("")
+
+  if prediction==0:
+    st.markdown('<p style="font-family:Calibri;font-size:40px; color:red;text-align:center;"><strong>Not Approved </strong> </p>', unsafe_allow_html=True)
+  elif prediction==2:
+    st.markdown('<p style="font-family:Calibri;font-size:40px; color:green;text-align:center;"><strong>Approved </strong> </p>', unsafe_allow_html=True)
+  else:
+    st.markdown('<p style="font-family:Calibri;font-size:40px; color:red;text-align:center;"><strong>Default Risk </strong></p>', unsafe_allow_html=True)
+
 
 
