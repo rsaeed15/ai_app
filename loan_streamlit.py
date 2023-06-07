74f1d2d342233e1de52c4a876c8be09c2612f0a8
Rifat Saeed <48490833+rsaeed15@users.noreply.github.com>
2023-06-07 13:24:48 -0700

    Update loan_streamlit.py

--- loan_streamlit.py
+++ loan_streamlit.py
@@ -95,12 +95,9 @@ with col1:
 
  df = pd.DataFrame(prediction_proba, columns = labels)
  st.write(df)	
- st.write("")
- st.write("")
-
-sns.barplot(x=labels,y=pred*100,order=['Safe Loan','Not Safe','Default'])
+ sns.barplot(x=labels,y=pred*100,order=['Safe Loan','Not Safe','Default'])
 # Add figure in streamlit app
-st.pyplot(fig)
+ st.pyplot(fig)
 
 with col2:
   st.write("")
