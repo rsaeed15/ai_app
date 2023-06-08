016a954bf52a2b747a518af97fa3759d76f8b2a6
Rifat Saeed <48490833+rsaeed15@users.noreply.github.com>
2023-06-07 22:54:34 -0700

    Update loan_streamlit.py

--- loan_streamlit.py
+++ loan_streamlit.py
@@ -61,7 +61,7 @@ def load_data(path):
     dataset = pd.read_csv(path)
     return dataset
 dataset = load_data('loan_data.csv.gz')
-dataset=dataset.iloc[0:5000,:]
+#dataset=dataset.iloc[0:5000,:]
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
